7f34231b4cbe93db92b5323e99e24d0b5dc07d6c
asus <asus@192.168.100.13>
2021-03-05 21:49:35 +0700

    Bài tập Xây dựng train - test set

--- WekaPro/src/wekapro/WekaPro.java
+++ WekaPro/src/wekapro/WekaPro.java
@@ -21,12 +21,19 @@ public class WekaPro {
 //        System.out.println(model);
 //        model.saveData("E:\\Data\\iris.arff");
 //        model.saveData2CSV("E:\\Data\\iris_CSV.csv");
-        MyAprioriModel model = new MyAprioriModel(
-                "C:\\Program Files\\Weka-3-8-5\\data\\weather.numeric.arff",
-                "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1",
-                "-R 2-3");
-        model.mineAssociationRules();
+//        MyAprioriModel model = new MyAprioriModel(
+//                "C:\\Program Files\\Weka-3-8-5\\data\\weather.numeric.arff",
+//                "-N 10 -T 0 -C 0.9 -D 0.05 -U 1.0 -M 0.1 -S -1.0 -c -1",
+//                "-R 2-3");
+//        model.mineAssociationRules();
+//        System.out.println(model);
+        MyKnowledgeModel model = new MyKnowledgeModel(
+                "C:\\Program Files\\Weka-3-8-5\\data\\iris.arff", null, null);
+        model.trainset = model.divideTrainTest(model.dataset, 70, false);
+        model.testset = model.divideTrainTest(model.dataset, 70, true);
         System.out.println(model);
+        System.out.println(model.trainset.toSummaryString());
+        System.out.println(model.testset.toSummaryString());
     }
     
 }
